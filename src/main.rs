b4a19985e7004b0fcb5552a47c74bfdf32620ad5
Anorak <palimagus@outlook.fr>
2023-02-07 14:01:28 +0100

    Import log_button into app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use yew::prelude::*;
 
+use crate::components::log_button::LogButton;
+
+mod components {
+    pub mod log_button;
+}
+
 #[function_component(App)]
 fn app() -> Html {
     let danger_message_classes = vec!["bg-red-100", "text-red-500", "p-2", "rounded-md"];
@@ -9,6 +15,7 @@ fn app() -> Html {
             <p class={classes!(danger_message_classes)}>
                 { "Watchout! It's a wasm!" }
             </p>
+            <LogButton />
         </div>
     }
 }
