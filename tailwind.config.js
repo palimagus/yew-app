f8986fb42d85e60f4862f07051c8acc51c3f1c53
Anorak <palimagus@outlook.fr>
2023-02-25 21:46:20 +0100

    Some blue variants from the logo 🌊

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
         "./index.html",
     ],
     theme: {
-        extends: {}
+        extends: {
+            colors: {
+                "prime": {
+                    "100": "#C8D9FF",
+                    "200": "#A3B8FF",
+                    "300": "#7688FF",
+                    "400": "#3C46E4",
+                    "500": "#2B43DE",
+                    "600": "#252FC0",
+                    "700": "#0c156b",
+                    "800": "#08105A",
+                    "900": "#040C3F"
+                }
+            },
+        }
     },
     variants: {},
     plugins: [],
